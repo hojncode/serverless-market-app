915cff70582b8aa6d34e4445abdedb4d10c6ddb0
hojncode <glicm12@gmail.com>
2023-01-30 16:16:09 +0900

    css - 2nd box

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="grid gap-10 bg-slate-400 py-20 px-10">
+    <div className="grid gap-10 bg-slate-400 py-20 px-20">
       <div className="rounded-3xl bg-white p-6 shadow-xl ">
         <span className="text-3xl font-semibold">Select Item</span>
         <div className="my-2 flex justify-between text-gray-500">
@@ -21,9 +21,30 @@ const Home: NextPage = () => {
           Checkout
         </div>
       </div>
-      <div className="rounded-2xl bg-white p-10 shadow-2xl"></div>
-      <div className="rounded-2xl bg-white p-10 shadow-2xl"></div>
-      <div className="rounded-2xl bg-white p-10 shadow-2xl"></div>
+      <div className="overflow-hidden rounded-3xl bg-white shadow-2xl">
+        <div className="bg-blue-500 p-6 pb-14">
+          <span className="text-2xl text-white">Profile</span>
+        </div>
+        <div className="relative -top-5 rounded-3xl bg-white p-6">
+          <div className="relative -top-16 flex items-end justify-between">
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Orders</span>
+              <span className="font-medium">340</span>
+            </div>
+            <div className="h-24 w-24 rounded-full bg-red-400" />
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Spent</span>
+              <span className="font-medium">$340</span>
+            </div>
+          </div>
+        </div>
+        <div className="relative -top-16 -mt-10 -mb-5 flex flex-col items-center">
+          <span className="text-lg font-medium">Tony Molloy</span>
+          <span className="text-sm text-gray-500">미국</span>
+        </div>
+      </div>
+      <div className="rounded-3xl bg-white p-10 shadow-2xl"></div>
+      <div className="rounded-3xl bg-white p-10 shadow-2xl"></div>
     </div>
   );
 };
