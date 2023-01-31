6a7435d321b6e8cfb4b1dad39596e979d6750cc6
hojncode <glicm12@gmail.com>
2023-01-31 15:02:24 +0900

    decimal , marker

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,22 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <div className="flex flex-col space-y-2  p-5 ">
-      <details className="select-none open:bg-indigo-400 open:text-white">
-        <summary className="cursor-pointer">What is my fav. food.</summary>
-        <span className="selection:bg-indigo-600 selection:text-white">
-          rice
-        </span>
-      </details>
+      <ul className="list-decimal marker:text-teal-500">
+        <li>hi</li>
+        <li>hi</li>
+        <li>hi</li>
+      </ul>
     </div>
+
+    // detail 태그
+    // <div className="flex flex-col space-y-2  p-5 ">
+    //   <details className="select-none open:bg-indigo-400 open:text-white">
+    //     <summary className="cursor-pointer">What is my fav. food.</summary>
+    //     <span className="selection:bg-indigo-600 selection:text-white">
+    //       rice
+    //     </span>
+    //   </details>
+    // </div>
 
     // 두번째 css
     // <form className="flex flex-col space-y-2  p-5 ">
