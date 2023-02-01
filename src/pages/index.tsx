33068ef8df47d9191f90caaec215a8c0903e7b34
hojncode <glicm12@gmail.com>
2023-02-01 13:04:29 +0900

    가입페이지 생성 (enter.tsx)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return (
-    <div className="dark bg-[url('/img/css1.png')]">
-      <h2 className="text-[1000px] text-[#999]">hihiih</h2>
-    </div>
-  );
+  return null;
 };
 
 export default Home;
