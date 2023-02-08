9e89f213495b41be1a1718796f2b56babb0d56c0
hojncode <glicm12@gmail.com>
2023-02-08 19:16:27 +0900

    feat: create

---
+++ src/pages/live/[id].tsx
@@ -0,0 +1,56 @@
+import type { NextPage } from "next";
+
+const LiveDetail: NextPage = () => {
+  return (
+    <div className="space-y-4 py-10 px-4">
+      <div className="aspect-video w-full rounded-md bg-slate-300 shadow-md" />
+      <h3 className=" mt-2 text-2xl font-semibold text-gray-800 ">
+        Let`s try market
+      </h3>
+      <div className="mt-10 h-[50vh] space-y-4 overflow-y-scroll  py-16 px-4">
+        {/* {메세지들} */}
+        <div className="flex items-start space-x-2">
+          <div className="h-8 w-8 rounded-full bg-slate-500" />
+          <div className="w-1/2 rounded-md border border-gray-300 p-2 text-sm text-gray-700">
+            <p>Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
+          <div className="h-8 w-8 rounded-full bg-slate-500" />
+          <div className="w-1/2 rounded-md border border-gray-300 p-2 text-sm text-gray-700">
+            <p> I want ￦20,000</p>
+          </div>
+        </div>
+
+        <div className="flex items-start space-x-2">
+          <div className="h-8 w-8 rounded-full bg-slate-500" />
+          <div className="w-1/2 rounded-md border border-gray-300 p-2 text-sm text-gray-700">
+            <p>Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
+          <div className="h-8 w-8 rounded-full bg-slate-500" />
+          <div className="w-1/2 rounded-md border border-gray-300 p-2 text-sm text-gray-700">
+            <p> I want ￦20,000</p>
+          </div>
+        </div>
+      </div>
+      <div className="fixed inset-x-0 bottom-2  mx-auto w-full max-w-md">
+        {/*left-0 right-0 는 inset-x-0 과 같다. - 중앙 정렬을 하는 방법.*/}
+        <div className="relative flex items-center">
+          <input
+            type="text"
+            className="w-full rounded-full border-gray-300 pr-12 shadow-sm focus:border-orange-500 focus:outline-none focus:ring-orange-500"
+          />
+          <div className="absolute inset-y-0 right-0 flex py-1.5 pr-1.5">
+            <button className="flex cursor-pointer items-center rounded-full bg-orange-500 px-3 text-sm text-white hover:bg-orange-600 focus:ring-2 focus:ring-orange-500 focus:ring-offset-2">
+              &rarr;
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LiveDetail;
