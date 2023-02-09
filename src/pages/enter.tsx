b1d5b8484d00035969569cf26972c7959b5fa546
hojncode <glicm12@gmail.com>
2023-02-09 14:56:10 +0900

    Fix: classname 상태 관리, 컴포넌트화

--- src/pages/enter.tsx
+++ src/pages/enter.tsx
@@ -1,8 +1,5 @@
+import { cls } from "@/libs/utils";
 import { useState } from "react";
-
-function cls(...classnames: string[]) {
-  return classnames.join("");
-}
 
 export default function Enter() {
   const [method, setMethod] = useState<"email" | "phone">("email");
