f788eabf4c1f37f9822ca0530dd4ed532824dc4a
hojncode <glicm12@gmail.com>
2023-02-09 13:12:24 +0900

    fix: 반응형 사이즈 - 모바일 사이즈로 고정하기.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,5 +2,10 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="mx-auto w-full max-w-lg">
+      {/* max-w-lg 는 모바일 사이즈 고정 시킴. (반응형) */}
+      <Component {...pageProps} />
+    </div>
+  );
 }
