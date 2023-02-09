16d8487f3f190945d4448b716c481af606ac4788
hojncode <glicm12@gmail.com>
2023-02-09 15:40:04 +0900

    Fix: layout의 title 가운데 고정 (_app.tsx, Layout.tsx 의 max-w-xl 통일시켜야됨 )

--- src/component/Layout.tsx
+++ src/component/Layout.tsx
@@ -16,12 +16,12 @@ export default function Layout({
 }: LayoutProps) {
   return (
     <div>
-      <div className=" fixed top-0 flex w-full items-center justify-center border-b bg-white py-3 text-lg font-medium text-gray-800 ">
+      <div className="fixed top-0 flex w-full max-w-xl items-center justify-center  border-b bg-white py-3 text-lg font-medium text-gray-800">
         {title ? <span>{title}</span> : null}
       </div>
       <div className={cls("pt-16", hasTabBar ? "pb-16" : "")}>{children}</div>
       {hasTabBar ? (
-        <nav className="fixed bottom-0 flex items-center justify-between border-t bg-white pb-10 pt-3 text-gray-800">
+        <nav className="fixed bottom-0 flex max-w-xl items-center justify-between border-t bg-white pb-10 pt-3 text-gray-800">
           <span></span>
           <span></span>
           <span></span>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="mx-auto w-full max-w-lg">
+    <div className="mx-auto w-full max-w-xl">
       {/* max-w-lg 는 모바일 사이즈 고정 시킴. (반응형) */}
       <Component {...pageProps} />
     </div>
