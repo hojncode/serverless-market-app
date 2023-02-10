8b162bb46f893bb9664eecdb2422ec92eb1a33c5
hojncode <glicm12@gmail.com>
2023-02-10 11:17:15 +0900

    Feat: (api) API Routes - Nextjs's server-side https://nextjs.org/docs/api-routes/introduction

---
+++ src/pages/api/client-test.tsx
@@ -0,0 +1,17 @@
+import client from "@/libs/client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  client.user.create({
+    data: {
+      email: "hi",
+      name: "hi2",
+    },
+  });
+  res.json({
+    ok: true,
+  });
+}
