c8d49eec0f351146e628b9eb5a4efad11c14b69d
hojncode <glicm12@gmail.com>
2023-02-10 13:57:32 +0900

    Feat: react-hook-form) useForm()/ handleSubmit & register

--- src/pages/forms.tsx
+++ src/pages/forms.tsx
@@ -10,23 +10,34 @@
 import { useForm } from "react-hook-form";
 
 export default function Forms() {
-  const { register, watch } = useForm();
+  const { register, handleSubmit } = useForm();
+  const onValid = () => {
+    console.log("VALID");
+  };
   //   console.log(register("name"));
   //   console.log(watch());
   return (
-    <form>
+    <form onSubmit={handleSubmit(onValid)}>
       <input
-        {...register("username")}
+        {...register("username", {
+          required: true,
+        })}
         type="text"
         placeholder="Username"
-        required
       />
-      <input {...register("email")} type="email" placeholder="Email" required />
       <input
-        {...register("password")}
+        {...register("email", {
+          required: true,
+        })}
+        type="email"
+        placeholder="Email"
+      />
+      <input
+        {...register("password", {
+          required: true,
+        })}
         type="password"
         placeholder="Password"
-        required
       />
       <input type="submit" value="Create Account" />
     </form>
