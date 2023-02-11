3b43ac7256a2578fd05ad5b2a6a1d124e99f0854
hojncode <glicm12@gmail.com>
2023-02-11 22:54:17 +0900

    Feat: react-hook-form) useForm - reset, error message, watch, setError, setValue

--- src/pages/forms.tsx
+++ src/pages/forms.tsx
@@ -7,24 +7,34 @@
  * TODO:Easier Inputs
  */
 
+import { useState } from "react";
 import { FieldErrors, useForm } from "react-hook-form";
 
 interface LoginForm {
   username: string;
   password: string;
   email: string;
+  errors?: string;
 }
 
 export default function Forms() {
+  const [enter, setEnter] = useState("");
   const {
     register,
     handleSubmit,
+    watch,
+    setError,
+    setValue,
+    reset,
+    resetField,
     formState: { errors },
   } = useForm<LoginForm>({
     // mode: "onBlur", // 해당 태그를 벗어나면 바로 설정한 에러메세지를 띄워줌.
     mode: "onChange", // 해당 태그 입력시 바로 설정한 에러메세지를 띄워줌.
   });
   const onValid = (data: LoginForm) => {
+    reset();
+    setEnter(`Welcome ${data.username} , your Email: ${data.email}`);
     console.log("VALID");
   };
   //   console.log(register("name"));
@@ -34,7 +44,10 @@ export default function Forms() {
   };
   console.log(errors);
   return (
-    <form onSubmit={handleSubmit(onValid, onInvalid)}>
+    <form
+      className="mx-5 my-5 flex flex-col px-10"
+      onSubmit={handleSubmit(onValid, onInvalid)}
+    >
       <input
         {...register("username", {
           required: "Username is required",
@@ -46,27 +59,38 @@ export default function Forms() {
         type="text"
         placeholder="Username"
       />
+      {errors.username?.message}
       <input
         {...register("email", {
           required: "Email is required",
           validate: {
             notGmail: (value) =>
-              !value.includes("@gmail.com") || "Gmail is not allowed",
+              !value.includes("@gmail.com") || "gmail is not allowed",
           },
         })}
         type="email"
-        placeholder="Email"
+        placeholder="Email (@gmail.com is not allowed)"
         className={`${Boolean(errors.email?.message) ? "border-red-500" : ""}`}
       />
       {errors.email?.message}
       <input
         {...register("password", {
           required: "Password is required",
+          minLength: {
+            message: "over 4 chars/num",
+            value: 4,
+          },
         })}
         type="password"
         placeholder="Password"
+        className={`${
+          Boolean(errors.password?.message) ? "border-red-500" : ""
+        }`}
       />
+      {errors.password?.message}
       <input type="submit" value="Create Account" />
+      {errors.errors?.message}
+      {enter}
     </form>
   );
 }
