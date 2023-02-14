a4adb82da4b60d5618b818d4a10dc0e789c7099e
hojncode <glicm12@gmail.com>
2023-02-14 20:29:22 +0900

    feat: Twilio SMS Message - .env 환경변수에 twilio 에서 발급한 id값들 추가, message.create() 작성하여 SMS 발송

--- src/pages/api/users/enter.tsx
+++ src/pages/api/users/enter.tsx
@@ -1,6 +1,9 @@
 import client from "@/libs/server/client";
 import withHandler, { ResponseType } from "@/libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
+import twilio from "twilio";
+
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(
   req: NextApiRequest, //!!!!! req 의 인코딩 기준으로 인코딩 된다. 많은 next.js 개발자들이 놓치는 부분이다.
@@ -122,6 +125,15 @@ async function handler(
   //   }
   //   console.log(user);
   // }
+
+  if (phone) {
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      to: process.env.MY_PHONE!, // `!`를 붙임으로써 확실하게 존재하는 변수라고 타입스크립트에게 알려준다.
+      body: `Your login token is ${payload}`,
+    });
+    console.log("TWILIO!!", message);
+  }
 
   console.log("req", req.body); // FrontEnd 에서 (여기서 enter.jsx) headers를 추가 해주어야 .email 을 BackEnd 에서 받을 수 있다.(headers 입력 안할 시, req.body 까지만 백에서 받을 수 있다)
   // return res.status(200).end();
