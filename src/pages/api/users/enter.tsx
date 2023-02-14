d7dfdf0744c98cfc1c97a9fc4a113f2b7b92d2a4
hojncode <glicm12@gmail.com>
2023-02-14 16:39:30 +0900

    Refactoring: payload - 랜덤 숫자 생성, user - phone/email 존재 여부 확인후 connectOrCreate 로 토큰 생성 || TypeScript - ResponseType 생성

--- src/pages/api/users/enter.tsx
+++ src/pages/api/users/enter.tsx
@@ -1,15 +1,17 @@
 import client from "@/libs/server/client";
-import withHandler from "@/libs/server/withHandler";
+import withHandler, { ResponseType } from "@/libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
 async function handler(
   req: NextApiRequest, //!!!!! req 의 인코딩 기준으로 인코딩 된다. 많은 next.js 개발자들이 놓치는 부분이다.
-  res: NextApiResponse
+  res: NextApiResponse<ResponseType>
 ) {
   const { phone, email } = req.body; //? phone 에 Int 최댓값(2147483647)보다 크게 입력하면 오류가 발생한다.
 
   /**방법 3. payload 로 정리. */
-  const payload = phone ? { phone: +phone } : { email }; // +phone 숫자형.
+  const user = phone ? { phone: +phone } : email ? { email } : null;
+  if (!user) return res.status(400).json({ ok: false });
+  const payload = Math.floor(100000 + Math.random() * 900000) + ""; // "" String 형.
   // const user = await client.user.upsert({
   //   where: {
   //     ...payload,
@@ -22,15 +24,15 @@ async function handler(
   // });
   const token = await client.token.create({
     data: {
-      payload: Math.floor(100000 + Math.random() * 900000) + "", // "" String 형.
+      payload,
       user: {
         connectOrCreate: {
           where: {
-            ...payload,
+            ...user,
           },
           create: {
             name: "Anonymous",
-            ...payload,
+            ...user,
           },
         },
       },
@@ -122,7 +124,8 @@ async function handler(
   // }
 
   console.log("req", req.body); // FrontEnd 에서 (여기서 enter.jsx) headers를 추가 해주어야 .email 을 BackEnd 에서 받을 수 있다.(headers 입력 안할 시, req.body 까지만 백에서 받을 수 있다)
-  return res.status(200).end(); //  == res.json({ ok: true });
+  // return res.status(200).end();
+  return res.json({ ok: true });
 }
 
 export default withHandler("POST", handler); // 첫번째 인자에 들어가는 값에따라 , 두번째 인자에 들어가는 함수에서 설정한 값의 페이지로 이동시킬 수 있다. (여기서는 , handler 에서 요청이 Post 이면 api/user/enter 접속시에 405 에러 페이지로 보내도록 설정함)
