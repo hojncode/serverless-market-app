4b55851aa29afdd7a1a7b994ba5cfc73b980c3ca
hojncode <glicm12@gmail.com>
2023-02-15 17:11:27 +0900

    Refactoring : 쿠키이름/비번 & TypeScript (declare module) 한곳에서 export 보내도록 수정

---
+++ src/libs/server/withSession.tsx
@@ -0,0 +1,18 @@
+import { withIronSessionApiRoute } from "iron-session/next";
+
+declare module "iron-session" {
+  interface IronSessionData {
+    user?: {
+      id: number;
+    };
+  }
+}
+
+const cookieOptions = {
+  cookieName: "carrotsession",
+  password: process.env.COOKIE_PASSWORD!, // COOKIE_PASSWORD! ---> ! 붙이는 이유는 환경변수에서 가져와야하는데 undefined 일 수 있기때문에 !를 붙여서 무조건 값이 있다고 타입 설정을 해둔다.
+};
+
+export function withApiSession(fn: any) {
+  return withIronSessionApiRoute(fn, cookieOptions);
+}
--- src/pages/api/users/me.tsx
+++ src/pages/api/users/me.tsx
@@ -1,39 +1,22 @@
-import { withIronSessionApiRoute } from "iron-session/next";
 import client from "@/libs/server/client";
 import withHandler, { ResponseType } from "@/libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
-
-declare module "iron-session" {
-  interface IronSessionData {
-    user?: {
-      id: number;
-    };
-  }
-}
+import { withApiSession } from "@/libs/server/withSession";
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
   console.log(req.session.user);
-  //   const profile = JSON.stringify(
-  //     await client.user.findUnique({
-  //       where: { id: req.session.user?.id },
-  //     })
-  //   );
-  //   console.log(profile);
-  //   //   res.status(200).end();
-
-  //   const updatedData = JSON.stringify(profile, (_key, value) => {
-  //     typeof value === "bigint" ? (value = value.toString()) : value.toString();
-  //   });
-
+  const profile = await client.user.findUnique({
+    where: { id: req.session.user?.id },
+  });
+  console.log(profile);
+  //   res.status(200).end();
   res.json({
     ok: true,
+    profile,
   });
 }
 
-export default withIronSessionApiRoute(withHandler("GET", handler), {
-  cookieName: "carrotsession",
-  password: "0980008801230323122301fdfefafsdafsafsadfs",
-}); // 첫번째 인자에 들어가는 값에따라 , 두번째 인자에 들어가는 함수에서 설정한 값의 페이지로 이동시킬 수 있다. (여기서는 , handler 에서 요청이 Post 이면 api/user/enter 접속시에 405 에러 페이지로 보내도록 설정함)
+export default withApiSession(withHandler("GET", handler));
