1d61cd07d51d2a7d2bda7a0761bd2e5f7f08e3dc
hojncode <glicm12@gmail.com>
2023-02-15 21:43:34 +0900

    feat: SWR 설치 - SWR 은 캐시에 저장된 데이터를 가져온 후 , fetch 요청을 통해 재검증 하고, 최종적으로 최신 데이터를 가져옴 ,React Query 비슷한 역할 (용량의 차이, 프로젝트에 맞게 선택해서 사용하면 된다)

--- src/libs/client/useUser.tsx
+++ src/libs/client/useUser.tsx
@@ -1,18 +1,12 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+const fetcher = (url: string) => fetch(url).then((response) => response.json());
 
 export default function useUser() {
-  const [user, setUser] = useState();
-  const router = useRouter();
-  useEffect(() => {
-    fetch("/api/users/me")
-      .then((response) => response.json())
-      .then((data) => {
-        if (!data.ok) {
-          return router.replace("/enter");
-        }
-        setUser(data.profile);
-      });
-  }, [router]);
-  return user;
+  //   const [user, setUser] = useState();
+  const { data, error, mutate } = useSWR("/api/users/me", fetcher);
+
+  return data;
 }
