9db80370072e2cc000285221df929fbe073a4725
hojncode <glicm12@gmail.com>
2023-02-15 17:13:34 +0900

    Feat: 로그인후 페이지 리다이렉션(useEffect, useRouter 사용) || 로그인 후 클라이언트의 토큰정보 삭제 (deleteMany 사용)

--- src/pages/api/users/confirm.tsx
+++ src/pages/api/users/confirm.tsx
@@ -1,15 +1,7 @@
-import { withIronSessionApiRoute } from "iron-session/next";
 import client from "@/libs/server/client";
 import withHandler, { ResponseType } from "@/libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
-
-declare module "iron-session" {
-  interface IronSessionData {
-    user?: {
-      id: number;
-    };
-  }
-}
+import { withApiSession } from "@/libs/server/withSession";
 
 async function handler(
   req: NextApiRequest,
@@ -18,22 +10,25 @@ async function handler(
   // console.log("req.session!!", req.session);
   const { token } = req.body;
   console.log("token!!", token);
-  const exists = await client.token.findUnique({
+  const foundToken = await client.token.findUnique({
     where: {
       payload: token,
     },
     // include: { user: true },
   });
-  if (!exists) return res.status(404).end();
-  // console.log("exists!!", exists);
+  if (!foundToken) return res.status(404).end();
+  // console.log("foundToken!!", foundToken);
   req.session.user = {
-    id: exists.userId,
+    id: foundToken.userId,
   };
   await req.session.save();
-  res.status(200).end();
+  await client.token.deleteMany({
+    where: {
+      userId: foundToken.userId,
+    },
+  });
+  // res.status(200).end();
+  res.json({ ok: true });
 }
 
-export default withIronSessionApiRoute(withHandler("POST", handler), {
-  cookieName: "carrotsession",
-  password: "0980008801230323122301fdfefafsdafsafsadfs",
-}); // 첫번째 인자에 들어가는 값에따라 , 두번째 인자에 들어가는 함수에서 설정한 값의 페이지로 이동시킬 수 있다. (여기서는 , handler 에서 요청이 Post 이면 api/user/enter 접속시에 405 에러 페이지로 보내도록 설정함)
+export default withApiSession(withHandler("POST", handler));
--- src/pages/enter.tsx
+++ src/pages/enter.tsx
@@ -2,7 +2,8 @@ import Button from "@/components/button";
 import Input from "@/components/input";
 import useMutation from "@/libs/client/useMutation";
 import { cls } from "@/libs/client/utils";
-import { useState } from "react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 
 interface EnterForm {
@@ -56,9 +57,17 @@ export default function Enter() {
     confirmToken(validForm);
     console.log("validForm!!", validForm);
   };
+  /** useRouter */
+  const router = useRouter();
+  /** useEffect */
+  useEffect(() => {
+    if (tokenData?.ok) {
+      router.push("/");
+    }
+  }, [tokenData, router]);
 
-  console.log(loading, data, error);
-  console.log("datatatata", data);
+  // console.log(loading, data, error);
+  // console.log("datatatata", data);
   return (
     <div className="mt-16 px-4">
       <h3 className="text-center text-3xl font-bold">Enter to Carrot</h3>
