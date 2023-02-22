31f2113375b0eaed4cc40173088426db842e5b05
hojncode <glicm12@gmail.com>
2023-02-22 23:12:44 +0900

    feat:Add posting , 입력한 텍스트 db 에 저장

--- src/pages/community/write.tsx
+++ src/pages/community/write.tsx
@@ -1,14 +1,44 @@
 import Button from "@/components/button";
 import Layout from "@/components/layout";
 import TextArea from "@/components/textarea";
+import useMutation from "@/libs/client/useMutation";
+import { Post } from "@prisma/client";
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+
+interface WriteForm {
+  question: string;
+}
+
+interface WriteResponse {
+  ok: boolean;
+  post: Post;
+}
 
 const Write: NextPage = () => {
+  const router = useRouter()
+  const { register, handleSubmit } = useForm<WriteForm>();
+  const [post, { loading, data }] = useMutation<WriteResponse>("/api/posts");
+  const onValid = (data: WriteForm) => {
+    if (loading) return; // 이미 작동중 (로딩중) 일때 유저가 게속 클릭해서 여러번 제출하게 되는것을 방지하기위함.
+    post(data);
+  };
+  useEffect(() => {
+    if (data && data.ok) {
+      router.push(`/community/${data.post.id}`)
+    }
+  }, [data,router]);
   return (
     <Layout canGoBack title="Write Post">
-      <form className="space-y-4 p-4">
-        <TextArea required placeholder="Ask a question!" />
-        <Button text="Submit" />
+      <form onSubmit={handleSubmit(onValid)} className="space-y-4 p-4">
+        <TextArea
+          register={register("question", { required: true, minLength: 5 })}
+          required
+          placeholder="Ask a question!"
+        />
+        <Button text={loading ? "Loading..." : "Submit"} />
       </form>
     </Layout>
   );
