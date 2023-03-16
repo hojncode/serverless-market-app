1eff06f0e4bf3e25d5a67afb59cb36822fdce4d6
hojncode <glicm12@gmail.com>
2023-03-16 17:57:27 +0900

    fix: 로딩메세지 (isLoading 또는 data === undefined 일 경우) 텍스트 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,42 +21,49 @@ interface ProductsResponse {
 const Home: NextPage = () => {
   const { user, isLoading } = useUser();
   const { data } = useSWR<ProductsResponse>("/api/products");
-  console.log(data);
-  // console.log("(indx.tsx)user!!!", user);
+  console.log("Home/data!!!", data);
+  console.log("Home/user!!!", user);
+  console.log("Home/isLoading!!!", isLoading);
+
   return (
     <Layout hasTabBar title="홈">
       <Head>
         <title>홈:Home</title>
       </Head>
-      <div className="flex flex-col space-y-5 divide-y ">
-        {data?.products?.map((product) => (
-          <Item
-            id={product.id}
-            key={product.id}
-            title={product.name}
-            price={product.price}
-            comments={1} //?
-            hearts={product._count.favs}
-          />
-        ))}
-        <FloatingButton href="/products/upload">
-          <svg
-            className="h-6 w-6"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+      {isLoading ? (
+        "로딩중..."
+      ) : (
+        <div className="flex flex-col space-y-5 divide-y ">
+          {data?.products?.map((product) => (
+            <Item
+              image={product.image}
+              id={product.id}
+              key={product.id}
+              title={product.name}
+              price={product.price}
+              comments={1} //?
+              hearts={product._count.favs}
             />
-          </svg>
-        </FloatingButton>
-      </div>
+          ))}
+          <FloatingButton href="/products/upload">
+            <svg
+              className="h-6 w-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+              />
+            </svg>
+          </FloatingButton>
+        </div>
+      )}
     </Layout>
   );
 };
