c483b830d199c316141fc984efc83c8acdc98bb9
hojncode <glicm12@gmail.com>
2023-03-20 15:57:22 +0900

    fix: error

--- src/pages/api/users/me/index.ts
+++ src/pages/api/users/me/index.ts
@@ -3,7 +3,6 @@ import client from "@/libs/server/client";
 import withHandler, { ResponseType } from "@/libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 import { withApiSession } from "@/libs/server/withSession";
-import { is } from "immer/dist/internal";
 
 async function handler(
   req: NextApiRequest,
