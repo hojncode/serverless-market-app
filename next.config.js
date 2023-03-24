3bdad3ce6711d559dff3263a92e8f9a325b2aa22
hojncode <glicm12@gmail.com>
2023-03-24 15:40:04 +0900

    fix - searching login error - nextconfig domain

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  domains: ["imagedelivery.net", "videodelivery.net", "cloudflarestream.com"],
-  // remotePatterns: [
-  //   {
-  //     protocol: "https",
-  //     hostname: "imagedelivery.net/",
-  //     port: "",
-  //     pathname: "",
-  //   },
-  // ],
+  // domains: ["imagedelivery.net", "videodelivery.net", "cloudflarestream.com"],
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "imagedelivery.net/",
+      port: "",
+      pathname: "",
+    },
+  ],
 };
 
 module.exports = nextConfig;
