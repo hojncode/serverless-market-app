61859a3a7ea816e44a9e50f8c2b3ab33ff7db247
hojncode <glicm12@gmail.com>
2023-03-25 19:31:32 +0900

    searching error - nextconfig - content-type

--- next.config.js
+++ next.config.js
@@ -27,6 +27,10 @@ const nextConfig = {
             value:
               "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
           },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
         ],
       },
     ];
