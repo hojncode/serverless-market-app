35a84bb1785bee2fa1584cdcfa8f0b84afa0a3f0
hojncode <glicm12@gmail.com>
2023-03-28 16:42:23 +0900

    searching error - iron session

--- src/libs/server/withSession.tsx
+++ src/libs/server/withSession.tsx
@@ -12,6 +12,9 @@ const cookieOptions = {
   secure: true,
   cookieName: "carrotsession",
   password: process.env.COOKIE_PASSWORD!, // COOKIE_PASSWORD! ---> ! 붙이는 이유는 환경변수에서 가져와야하는데 undefined 일 수 있기때문에 !를 붙여서 무조건 값이 있다고 타입 설정을 해둔다.
+  cookieOptions: {
+    secure: process.env.NODE_ENV === "production",
+  },
 };
 
 export function withApiSession(fn: any) {
