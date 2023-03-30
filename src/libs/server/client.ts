575b197ee5a2b45b5930f9ecd9743a65241666a4
hojncode <glicm12@gmail.com>
2023-03-30 12:21:17 +0900

    searching error : client.ts

--- src/libs/server/client.ts
+++ src/libs/server/client.ts
@@ -4,8 +4,8 @@ declare global {
   var client: PrismaClient | undefined;
 }
 
-const client = global.client || new PrismaClient({ log: ["query"] });
+const client = global.client || new PrismaClient(); // 쿼리 확안을 원할시 -> new PrismaClient({ log: ["query"] })
 
-if (process.env.NODE_ENV === "development") global.client = client;
+if (process.env.NODE_ENV === "production") global.client = client;
 
 export default client;
