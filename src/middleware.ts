8ffe514a7ece904c3f8d94c8c4635f8015fa6f74
hojncode <glicm12@gmail.com>
2023-04-03 18:18:52 +0900

    feat:  Responses and Redirections

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,23 @@
-import type { NextRequest } from "next/server";
+import { NextRequest, NextResponse, userAgent } from "next/server";
 
 export function middleware(req: NextRequest) {
-  console.log("미들웨어다!!!");
-  console.log(req.cookies);
+  if (userAgent(req).isBot) {
+    return new Response("You are Bot", { status: 403 });
+  }
+  const ua = userAgent(req); // 현재 버전에서 userAgent 가져오는 법.
+  console.log("USERAGENT!!", ua);
+  console.log(req.url);
+
+  //  if (!req.url.includes("/api")) 으로 감싸는 이유 -> 로그인 되지않은 상태이기 때문에 api 정보를 요청할 필요가 없다(로그인되어야 유저의 api정보로 로그인유무를 파악하기 때문에)
+  if (!req.url.includes("/api")) {
+    if (!req.url.includes("/enter") && !req.cookies.has("carrotsession")) {
+      // req.nextUrl.searchParams.set("from", req.nextUrl.pathname);
+      req.nextUrl.pathname = "enter";
+      return NextResponse.redirect(req.nextUrl);
+    }
+  }
 }
+
+export const config = {
+  matcher: ["/((?!api|_next/static|favicon.ico).*)"],
+};
